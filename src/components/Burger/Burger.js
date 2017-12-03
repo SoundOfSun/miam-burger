a5d7af80bb79313b9d98e20096f4209fc25049b2
Cecile Alfred <cecile.alfredx@gmail.com>
2017-12-03 16:55:41 +0000

    Added logic to get sum of ingredients + backup message if 0 ingredient

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -8,12 +8,22 @@ const burger = ( props ) => {
 
   // change ingredients object into an array (see state)
   // .keys get the keys only to populate an array
-  const transformedIngredients = Object.keys(props.ingredients)
+  let transformedIngredients = Object.keys(props.ingredients)
     .map(ingKey => {
       return [...Array(props.ingredients[ingKey])].map((_, i) => {
         return <BurgerIngredient key={ingKey + i} type={ingKey} />
       });
-    });
+    })
+    // reduce is a js array func to transform an array into smtg else
+    .reduce((prevVal, currVal) => {
+      return prevVal.concat(currVal);
+    }, []);
+  console.log(transformedIngredients);
+  // Now we end up with empty array or array of jsx objects
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients !</p>
+  }
 
   // need div for style height/width
   return (
