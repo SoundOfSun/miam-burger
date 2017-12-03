19c3e4a08d4cc692873f533011337e44e5be04b6
Cecile Alfred <cecile.alfredx@gmail.com>
2017-12-03 19:43:24 +0000

    Created Order Summary component for the modal - need to link to btn

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import Aux from '../../../hoc/Aux';
+
+const orderSummary = ( props ) => {
+  // Here, curly braces because we'll add some logic
+
+  // Ingredients are objects, we need an array format to map
+  const ingredientSummary = Object.keys(props.ingredients)
+    .map(ingKey => {
+      return (
+        <li key={ingKey}>
+          <span style={{textTransform: 'capitalize'}}>{ingKey}</span>: {props.ingredients[ingKey]}
+        </li> );
+    });
+
+  return (
+    <Aux>
+      <h3>Your order</h3>
+      <p>A delicious burger with the following infredients:</p>
+      <ul>
+        {ingredientSummary}
+      </ul>
+      <p>Continue to checkout ?</p>
+    </Aux>
+  );
+};
+
+export default orderSummary;
