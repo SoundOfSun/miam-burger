7c930956b4834e2591d5535299f51793b73d30ee
Cecile Alfred <cecile.alfredx@gmail.com>
2017-12-06 18:54:49 +0000

    Performance improve: ShouldUpdate on Modal to control re-rendering of Summary

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,37 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = ( props ) => {
+class OrderSummary extends Component {
   // Here, curly braces because we'll add some logic
+  // This could be a functional component
+  // It's stateful to get the debugging WillUpdate
 
-  // Ingredients are objects, we need an array format to map
-  const ingredientSummary = Object.keys(props.ingredients)
-    .map(ingKey => {
-      return (
-        <li key={ingKey}>
-          <span style={{textTransform: 'capitalize'}}>{ingKey}</span>: {props.ingredients[ingKey]}
-        </li> );
-    });
+  // Check when the Summary inside the modal updates : each time add ing.
+  componentWillUpdate() {
+    console.log('[OrderSummary] WillUpdate');
+  }
 
-  return (
-    <Aux>
-      <h3>Your order</h3>
-      <p>A delicious burger with the following infredients:</p>
-      <ul>
-        {ingredientSummary}
-      </ul>
-      <p><strong>Total price: {props.price.toFixed(2)}</strong></p>
-      <p>Continue to checkout ?</p>
-      <Button
-        btnType="Danger"
-        clicked={props.purchaseCancelled}>CANCEL</Button>
-      <Button
-        btnType="Success"
-        clicked={props.purchaseContinued}>CONTINUE</Button>
-    </Aux>
-  );
-};
+  render () {
+    // Ingredients are objects, we need an array format to map
+    const ingredientSummary = Object.keys(this.props.ingredients)
+      .map(ingKey => {
+        return (
+          <li key={ingKey}>
+            <span style={{textTransform: 'capitalize'}}>{ingKey}</span>: {this.props.ingredients[ingKey]}
+          </li> );
+      });
 
-export default orderSummary;
+    return (
+      <Aux>
+        <h3>Your order</h3>
+        <p>A delicious burger with the following infredients:</p>
+        <ul>
+          {ingredientSummary}
+        </ul>
+        <p><strong>Total price: {this.props.price.toFixed(2)}</strong></p>
+        <p>Continue to checkout ?</p>
+        <Button
+          btnType="Danger"
+          clicked={this.props.purchaseCancelled}>CANCEL</Button>
+        <Button
+          btnType="Success"
+          clicked={this.props.purchaseContinued}>CONTINUE</Button>
+      </Aux>
+    );
+  }
+}
+
+export default OrderSummary;
